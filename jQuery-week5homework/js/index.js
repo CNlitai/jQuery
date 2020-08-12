c31ce7e63ad3cc30563f2e30d699c7cf2da085c3
CNlitai <2083645366@qq.com>
2020-08-12 10:18:29 +0800

    Update index.js

--- jQuery-week5homework/js/index.js
+++ jQuery-week5homework/js/index.js
@@ -3,7 +3,7 @@ $(function(){
         click:function(){
         //    alert( $(this).text())
             if($(this).text()=='用户管理'){
-                $('.content').load('./pages/user.html')
+                $('.content').load('../pages/user.html')
             }else if($(this).text()=='栏目管理'){
                 $('.content').load('./pages/category.html')
                 
